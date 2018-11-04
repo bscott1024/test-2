74bb74f198e55011539154ca36ad7e34d19784fd
Bryan <Bryan@macbook-pro.home>
2018-11-03 22:03:25 -0400

    Created new file and created a simlple writeline/readline command

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter your name");
+			string string1;
+			string1 = Console.ReadLine();
+			Console.WriteLine(string1);
+
+		}
+	}
+}
